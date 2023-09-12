b52d08a98d2f488a74b080ecf86d91e1fadd855f
DevitoDbug <Davidochiengy@gmail.com>
2023-09-12 20:30:58 +0300

    Visit site

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("quotes.toscrape.com"),
+	)
+
+	c.OnRequest(func(request *colly.Request) {
+		//request.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36\n")
+		fmt.Println("We are visiting a URL", request.URL)
+	})
+
+	c.OnResponse(func(response *colly.Response) {
+		fmt.Println("Response Code", response.StatusCode)
+	})
+
+	c.OnError(func(response *colly.Response, err error) {
+		fmt.Println("error", err.Error())
+	})
+
+	err := c.Visit("http://quotes.toscrape.com/random")
+	if err != nil {
+		return
+	}
+}
